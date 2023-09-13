8dca4c43e9d03489d8935fbb285083a5deab8002
khaleddayem <125710888+khaleddayem@users.noreply.github.com>
2023-09-13 05:51:40 +0300

    Update main.c

--- VariadicFunctionsInC/main.c
+++ VariadicFunctionsInC/main.c
@@ -6,9 +6,9 @@
 * Author github         : https://github.com/khaleddayem
 
 * Problem Description   : In this problem, you will implement three variadic functions 
-						  to calculate sums, minima, maxima of a variable number of arguments. 
-						  The first argument passed is the count of the number of arguments, 
-						  which is followed by the arguments themselves.
+			  to calculate sums, minima, maxima of a variable number of arguments. 
+			  The first argument passed is the count of the number of arguments, 
+			  which is followed by the arguments themselves.
 
 * Problem Link          : https://www.hackerrank.com/challenges/variadic-functions-in-c
 
@@ -27,61 +27,64 @@
 
 
 int  sum (int count,...) {
-	/* declare a variable named "nums" that will hold the list of arguments passed to the function */
+    /* declare a variable named "nums" that will hold the list of arguments passed to the function */
     va_list(nums);
-	/* initialize the argument list with the given count */
+    /* initialize the argument list with the given count */
     va_start(nums,count);
-	/* Calculate the sum */
+    /* Calculate the sum */
     int x,sum=0;
     int i=0;
     for(i=0;i<count;i++)
     {
-	   /* retrieves a value of the given list located by arg_ptr and increases arg_ptr to point to the next argument in the list */
+       /* retrieves a value of the given list located by arg_ptr and increases arg_ptr to point to the next argument in the list */
        x=va_arg(nums,int);
        sum+=x;
     }
-	/* cleanup the list */
+    /* cleanup the list */
     va_end(nums);
     return sum;
 }
 
 int min(int count,...) {
 	
-	/* declare a variable named "nums" that will hold the list of arguments passed to the function */
-	va_list(nums);
-	/* initialize the argument list with the given count */
+    /* declare a variable named "nums" that will hold the list of arguments passed to the function */
+    va_list(nums);
+	
+    /* initialize the argument list with the given count */
     va_start(nums,count);
-	/* Getting the min value */
+	
+    /* Getting the min value */
     int x,min=MAX_INT_VALUE;
+	
     int i=0;
     for(i=0;i<count;i++)
     {
-	   /* retrieves a value of the given list located by arg_ptr and increases arg_ptr to point to the next argument in the list */		
+       /* retrieves a value of the given list located by arg_ptr and increases arg_ptr to point to the next argument in the list */		
        x=va_arg(nums,int);
        if(x<min)
        min=x;
     }
-	/* cleanup the list */
+    /* cleanup the list */
     va_end(nums);
     return min;
 }
 
 int max(int count,...) {
-	/* declare a variable named "nums" that will hold the list of arguments passed to the function */
+    /* declare a variable named "nums" that will hold the list of arguments passed to the function */
     va_list(nums);
-	/* initialize the argument list with the given count */
+    /* initialize the argument list with the given count */
     va_start(nums,count);
-	/* Getting the max value */
+    /* Getting the max value */
     int x,max=0;
     int i=0;
     for(i=0;i<count;i++)
     {
-	   /* retrieves a value of the given list located by arg_ptr and increases arg_ptr to point to the next argument in the list */				
+       /* retrieves a value of the given list located by arg_ptr and increases arg_ptr to point to the next argument in the list */				
        x=va_arg(nums,int);
        if(x>max)
        max=x;
     }
-	/* cleanup the list */
+    /* cleanup the list */
     va_end(nums);
     return max;
 }
