25908355db77d35cc1d979dfb3c78b85bf8db84b
khaleddayem <kh.dayem@gmail.com>
2023-09-13 04:35:20 +0300

    Added some comments to explain the code better

--- Digit Frequency/main.c
+++ Digit Frequency/main.c
@@ -1,22 +1,43 @@
+/************************************************************************************************
+* Author Name           : Khaled Abdeldayem
+
+* Author Email          : kh.dayem@gmail.com
+
+* Author github         : https://github.com/khaleddayem
+
+* Problem Description   : Given a string,consisting of alphabets and digits, 
+                          find the frequency of each digit in the given string.
+************************************************************************************************/
+
+/* INCLUDE SECTION */
+
 #include <stdio.h>
 #include <string.h>
 #include <math.h>
 #include <stdlib.h>
 
+/* MACRO SECTION */
+
+#define MAX_DIGIT 10
+#define ASCII_OF_0 48
+
+/* MAIN BODY */
+
 int main(){
     char* str;
-    int i=0;
-    str=malloc(1024 * sizeof(char));
-    scanf("%s",str);
-    str=realloc(str, strlen(str) + 1);
-    int freq[10]={0};
-    while(*str!='\0')
+    int iter=0;                             /* Used for printing the Freq array LOOP */
+	int freq[MAX_DIGIT]={0};                       /* Creating an Array of 0's for the freq */
+	
+    str=malloc(1024 * sizeof(char));        /* Allocating memory for the string */
+    scanf("%s",str);					    /* Scanning the string from the user */
+    str=realloc(str, strlen(str) + 1);      /* Reallocating the memory to free Unused memory */
+    while(*str!='\0')                       /* Looping through the string till the null char */
     {
         
-        freq[*str-48]++;
-        str++;
+        freq[*str-ASCII_OF_0]++;			/* Converting the numbers of Char to int by subtracting the ascii of zero (48) */
+        str++;								/* incrementing the value in the corresponding index to the number in string */
     }
-    for(i=0;i<10;i++)
+    for(iter=0;iter<MAX_DIGIT;iter++)       /* looping to print the array */
     {
         printf("%d ",freq[i]);
     }
